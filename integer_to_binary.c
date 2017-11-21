57810ffa05fe302cd3d5f574d162938066c91b80
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2017-11-21 07:50:43 +0100

    formatiert

--- integer_to_binary.c
+++ integer_to_binary.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
- 
-int main()
-{
-	int binary[16]={0};
-	int printloop;
-	int i=15;
-	int integer=0; 
-	int originalnumber=0;
 
-	printf("This Program shows up to 16 bits of a specified integer Number\n");
-	printf("Enter integer number\n");
-	scanf("%d",&integer);
-	
-	originalnumber=originalnumber+integer;
+int main() {
+  int binary[16] = {0};
+  int printloop;
+  int i = 15;
+  int integer = 0;
+  int originalnumber = 0;
 
-	while(integer>0){
-		if (integer%2!=0)
-			binary[i]=1;
+  printf("This Program shows up to 16 bits of a specified integer Number\n");
+  printf("Enter integer number\n");
+  scanf("%d", &integer);
 
-	i--;
-	integer=integer/2;
+  originalnumber = originalnumber + integer;
 
-	}
+  while (integer > 0) {
+    if (integer % 2 != 0)
+      binary[i] = 1;
 
-	printf("The binary representation of %d is:\n",originalnumber);
-	for(printloop = 0; printloop < 16; printloop++)
-	printf("%d ", binary[printloop]);
+    i--;
+    integer = integer / 2;
+  }
 
-return(0);
+  printf("The binary representation of %d is:\n", originalnumber);
+  for (printloop = 0; printloop < 16; printloop++)
+    printf("%d ", binary[printloop]);
+
+  return (0);
 }
